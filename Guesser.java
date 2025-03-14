2e5badc38540744ec2c272a6d645fb341f448a3c
jackwagneruni <jackwagneruni@gmail.com>
2025-03-14 22:04:30 +0000

    Finalizing some things

--- Guesser.java
+++ Guesser.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class Guesser {
     public static void main (String[] args) {
+        
         Scanner scanner_variable = new Scanner(System.in);
 
         boolean still_running = true;
-
+        
         while (still_running) {
             System.out.println("0: Stop running program");
             System.out.println("1: Human guessing mode");
             System.out.println("2: Computer guessing mode");
+
+            
             int menu_choice = scanner_variable.nextInt();
+            scanner_variable.nextLine();
             if (menu_choice == 0) {
                 // end the program 
                 still_running = false;
@@ -23,22 +27,29 @@ public class Guesser {
             } else if (menu_choice == 2) {
                 // call computer guesser mode class
                 computer_guesser(scanner_variable);
-            } else {
+            } else if (menu_choice != 0) {
                 // call computer guesser mode class
                 System.out.println("That is not a menu option, please type a number");
             } // end of menu choice if statements
+            
         } // end of menu loop
 
     } // end of main class
     public static void human_guesser (Scanner scanner_variable) {
+        int counter = 1;
         //System.out.println("You are now in the human guesser class");
         int correct_number = (int) (Math.random() * 100) + 1;
-        //System.out.println("Correct number is " + correct_number);
+        //System.out.println("Correct number is " + correct_number); // debugging purposes, shows correct answer on terminal
         boolean repeat_guessing_loop = true;
         while (repeat_guessing_loop) {
-            System.out.println("What is your guess?");
+            System.out.println(counter + ") What is your guess?");
             int current_guess = scanner_variable.nextInt();
             //System.out.println("You guessed: " + current_guess);
+            
+            if (counter >= 7) {
+                System.out.println("Your took longer than 7 guesses, if computer mode was used, it would have guessed by now");
+                repeat_guessing_loop = false;
+            }
             if (current_guess == correct_number) {
                 System.out.println("you got it!");
                 repeat_guessing_loop = false;
@@ -49,6 +60,7 @@ public class Guesser {
                 System.out.println("your guess was too small");
             } // end of if statements
 
+            counter++;
         } // end of human guessing number loop
 
     } // end of human guessing class
@@ -57,6 +69,7 @@ public class Guesser {
     
     
     public static void computer_guesser (Scanner scanner_variable) {
+        int counter = 1;
         //System.out.println("Now you are in the computer guesser class");
         int correct_number = 38;
         //System.out.println("Correct number is " + correct_number);
@@ -69,7 +82,7 @@ public class Guesser {
 
         while (repeat_variable) {
             int guess = (low + max) / 2;
-            System.out.println("My current guess is " + guess);
+            System.out.println(counter + ") My current guess is " + guess);
             System.out.println("If my guess is too low, respond with `L`");
             System.out.println("If my guess is too high, respond with `H`");
             System.out.println("If my guess correct , respond with `C`");
@@ -85,8 +98,7 @@ public class Guesser {
                 System.out.println("I got it correct. Woo hoo!");
                 repeat_variable = false;
             }
-
-
+            counter++;
 
         }
 
